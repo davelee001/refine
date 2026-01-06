cc664a22994cc3eb11bdee344a56208ef61d789b
David Leek <david.leekaleer@student.utamu.ac.ug>
2026-01-06 11:53:58 +0300

    docs: add helpful comment to noop function

--- examples/store/src/lib/noop.ts
+++ examples/store/src/lib/noop.ts
@@ -1,5 +1,5 @@
 /**
- * A funtion that does nothing.
+ * noop is a utility function that performs no operation. Useful as a default callback or placeholder.
  */
 export const noop = () => {
   return null;
